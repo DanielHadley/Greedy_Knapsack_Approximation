9718d73df3904cb589265f3646823105620b830b
DanielHadley <dphnrome@yahoo.com>
2014-04-08 14:03:36 -0400

    Almost done: need to add a break point and fix the df$add

---
+++ GreedyKnapsack.R
@@ -0,0 +1,47 @@
+# Created by Daniel Hadley
+# A greedy approximation algorithm to solve the 0/1 knapsack problem in R. 
+# Not optimal, but close enough for my purposes.
+
+# Create data to test the algo
+weight <- c(1, 3, 5, 7, 7, 8, 2, 1)
+value <- c(7, 2, 4, 3, 6, 4, 1, 1)
+
+
+
+# Write the algo as a function
+knapsack <- function(value, weight, limit){
+  benefit.to.cost <- value / weight #Create ratio
+  df = data.frame(value, weight, benefit.to.cost) # turn it into a DF
+  df <- df[with(df, order(-benefit.to.cost)), ] # Sort by benefit.to.cost
+  rownames(df) <- NULL # Reset the row names for easier indexing
+  df$Weight <- ifelse(cumsum(df$weight) <= limit, cumsum(df$weight), 0) # Add first items that fit
+  # I need to add a break here if nothing fits in the bag on the first pass
+  # Capital "W" Weight is the total weight of items, which must not exceed the limit
+  for(i in 2:nrow(df)){ #Start in row 2 because some values have been added above
+    df$Weight[i] <- ifelse(df$weight[i] + df$Weight[i-1] <= limit, # If adding won't go over limit
+                         df$weight[i] + df$Weight[i-1], df$Weight[i-1]) # If it will, keep Weight the same
+  }
+  df$add <- 0
+  df$add[1] <- ifelse(df$weight[1] > 0, 1, 0)
+  for(i in 2:nrow(df)){ #Start in row 2 
+    df$add[i] <- ifelse(df$weight[i] > df$Weight[i-1], 1, 0) # If adding won't go over limit
+  }
+  return(df)
+}
+
+knapsack(value, weight, 10)
+
+
+
+
+benefit.to.cost <- value / weight #Create ratio
+df = data.frame(value, weight, benefit.to.cost) # turn it into a DF
+df <- df[with(df, order(-benefit.to.cost)), ] # Sort by benefit.to.cost
+rownames(df) <- NULL # Reset the row names for easier indexing
+df$Weight <- ifelse(cumsum(df$weight) <= 10, cumsum(df$weight), 0) # Add first items that fit
+# I need to add a break here if nothing fits in the bag on the first pass
+# Capital "W" Weight is the total weight of items, which must not exceed the limit
+for(i in 2:nrow(df)){ #Start in row 2 because some values have been added above
+  df$Weight[i] <- ifelse(df$weight[i] + df$Weight[i-1] <= 10, # If adding won't go over limit
+                         df$weight[i] + df$Weight[i-1], df$Weight[i-1]) # If it will, keep Weight the same
+}
